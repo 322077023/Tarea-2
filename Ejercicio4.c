30e9dd4cb0cb0fa60e3f305c8db5d3ba28a23c63
322077023 <emilioalmaraz0@gmail.com>
2025-11-17 13:41:58 -0600

    Create Ejercicio4.c

---
+++ Ejercicio4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    float capital;
+    float tasa;      
+    float interes;
+    float monto;
+
+    printf("=== Calculo de monto al final del mes ===\n");
+
+    printf("Ingrese la cantidad a invertir: ");
+    scanf("%f", &capital);
+
+    printf("Ingrese la tasa de interes mensual (en %%): ");
+    scanf("%f", &tasa);
+
+    interes = capital * tasa / 100.0f;
+
+    monto = capital + interes;
+
+    printf("El monto al final del mes es: %.2f\n", monto);
+
+    return 0;
+}
