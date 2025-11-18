e11c28dfa673aceb4222c352b78b993c4133d162
322077023 <emilioalmaraz0@gmail.com>
2025-11-17 19:48:37 -0600

    Update Ejercicio9.c

--- Ejercicio9.c
+++ Ejercicio9.c
@@ -2,15 +2,16 @@
 
 int main() 
 {
-    int N, i;
+    int N, i = 1;
 
     printf("Ingrese un numero entero: ");
     scanf("%d", &N);
 
     printf("Tabla de multiplicar del %d:\n", N);
 
-    for (i = 1; i <= 10; i++) {
+    while (i <= 10) {
         printf("%d x %d = %d\n", N, i, N * i);
+        i++;   // incrementar
     }
 
     return 0;
