162cbe819608f9da5ad1ebbdea51be908a4a26d3
322077023 <emilioalmaraz0@gmail.com>
2025-11-17 19:46:25 -0600

    Create Ejercicio9.c

---
+++ Ejercicio9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    int N, i;
+
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &N);
+
+    printf("Tabla de multiplicar del %d:\n", N);
+
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", N, i, N * i);
+    }
+
+    return 0;
+}
